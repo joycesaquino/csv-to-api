f6b138dcfa4e25d5219edabaf2ae5d05772c9d17
joycesaquino <joycesaquino@gmail.com>
2022-09-14 11:32:05 -0300

    Implementing csv reader in main function

--- cmd/app.go
+++ cmd/app.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"csv-to-api/internal"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
+
 func main() {
+
+	csvFile, err := os.Open("visitor-event-test.csv")
+	var messages [][]string
+	reader := csv.NewReader(csvFile)
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Print(err)
+	}
+	messages = append(messages, records...)
+
+	converter := internal.NewConverter()
+	visitorEvents, err := converter.CsvToPriceHistory(messages)
+	if err != nil {
+		return
+	}
+	log.Println(visitorEvents)
 
 }
