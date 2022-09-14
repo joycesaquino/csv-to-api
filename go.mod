8bfa6b767af81f3310c86167b1ecb3a8ca282c51
joycesaquino <joycesaquino@gmail.com>
2022-09-14 11:25:27 -0300

    Implementing HTTP client

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module csv-to-api
 
 go 1.16
+
+require (
+	github.com/caarlos0/env/v6 v6.10.1
+	github.com/go-resty/resty/v2 v2.7.0
+)
--- internal/client.go
+++ internal/client.go
@@ -1,1 +1,65 @@
 package internal
+
+import (
+	"context"
+	"fmt"
+	"github.com/caarlos0/env/v6"
+	"github.com/go-resty/resty/v2"
+	"log"
+	"time"
+)
+
+type Config struct {
+	Url   string `env:"SERVICE_URL,notEmpty"`
+	Token string `env:"SERVICE_TOKEN,notEmpty"`
+}
+
+type Client struct {
+	restyClient *resty.Client
+}
+
+const (
+	contentTypeHeader = "Content-Type"
+	contentTypeJson   = "application/json"
+)
+
+func (client Client) Post(ctx context.Context, body VisitorEvent) error {
+
+	resp, err := client.restyClient.
+		R().
+		SetContext(ctx).
+		SetHeader(contentTypeHeader, contentTypeJson).
+		SetBody(body).
+		Post("/api-endpoint")
+
+	if err != nil {
+		return err
+	}
+
+	if resp.IsError() {
+		return fmt.Errorf("[ERROR] - StatusCode : %d , Error n: %s", resp.StatusCode(), err)
+	}
+
+	return nil
+}
+
+func NewClient() *Client {
+	var config Config
+	if err := env.Parse(&config); err != nil {
+		log.Fatalf("[ERROR] - Error on configure env on http client: %s", err)
+	}
+
+	client := resty.New()
+	client.RetryCount = 3
+	client.RetryWaitTime = 10 * time.Second
+	client.RetryConditions = []resty.RetryConditionFunc{
+		func(response *resty.Response, err error) bool {
+			return response.StatusCode() == 500
+		},
+	}
+	client.SetBaseURL(config.Url)
+	client.SetAuthScheme("Basic")
+	client.SetAuthToken(config.Token)
+
+	return &Client{restyClient: client}
+}
